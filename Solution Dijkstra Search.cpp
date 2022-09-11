a6b63fba78696efdec7710959a8352f7d618800a
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-09-11 09:13:56 +0200

    Create Solution Dijkstra Search.cpp

---
+++ Solution Dijkstra Search.cpp
@@ -0,0 +1,68 @@
+
+#include <array>
+#include <queue>
+#include <vector>
+using namespace std;
+
+class Solution {
+
+    struct Point {
+        int row;
+        int column;
+        int costFromStart;
+        Point(int row, int column, int costFromStart) : row{row}, column{column}, costFromStart{costFromStart}{}
+    };
+    inline static const auto comparator = [](const Point& x, const Point& y) {return x.costFromStart > y.costFromStart;};
+
+    inline static const int RIGHT_SIGN = 1;
+    inline static const int LEFT_SIGN = 2;
+    inline static const int UP_SIGN = 3;
+    inline static const int DOWN_SIGN = 4;
+    inline static const int COST_FOR_CHANGING_VALUE_OF_SIGN = 1;
+    inline static const array<array<int, 3>, 4> MOVES{{{0, 1, RIGHT_SIGN}, {0, -1, LEFT_SIGN}, {1, 0, UP_SIGN}, {-1, 0, DOWN_SIGN}}};
+
+    int rows;
+    int columns;
+
+public:
+    int minCost(vector<vector<int>>& grid) {
+        rows = grid.size();
+        columns = grid[0].size();
+        return dijkstraSearchForMinCostPath(grid);
+    }
+
+private:
+    int dijkstraSearchForMinCostPath(const vector<vector<int>>& grid) {
+
+        priority_queue<Point, vector<Point>, decltype(comparator) > minHeap(comparator);
+        vector<vector<int>> minCostPath(rows, vector<int>(columns, INT_MAX));
+
+        minCostPath[0][0] = 0;
+        minHeap.push(Point(0, 0, 0));
+
+        while (!minHeap.empty()) {
+            Point point = minHeap.top();
+            minHeap.pop();
+
+            for (const auto& move : MOVES) {
+                int nextRow = point.row + move[0];
+                int nextColumn = point.column + move[1];
+
+                if (pointIsInGrid(nextRow, nextColumn)) {
+                    int costFromCurrentToNextPoint = grid[point.row][point.column] != move[2] ? COST_FOR_CHANGING_VALUE_OF_SIGN : 0;
+                    int costFromStartToNextPoint = minCostPath[point.row][point.column] + costFromCurrentToNextPoint;
+
+                    if (costFromStartToNextPoint < minCostPath[nextRow][nextColumn]) {
+                        minCostPath[nextRow][nextColumn] = costFromStartToNextPoint;
+                        minHeap.push(Point(nextRow, nextColumn, costFromStartToNextPoint));
+                    }
+                }
+            }
+        }
+        return minCostPath[rows - 1][columns - 1];
+    }
+
+    int pointIsInGrid(int row, int column) const {
+        return row < rows && row >= 0 && column < columns && column >= 0;
+    }
+};
